6e29f28f7738bb668905220dfaa38e502afab5c8
yanhuiwu <2864281096@qq.com>
2023-03-20 00:51:51 +0800

    Update Camera.cpp
    limit pitch max, min value

--- openglProj/openglProj/Camera.cpp
+++ openglProj/openglProj/Camera.cpp
@@ -1,15 +1,14 @@
 ﻿#include "Camera.h"
+#include <iostream>
 
-
-
-Camera::Camera(glm::vec3 position, glm::vec3 target, glm::vec3 worldup)
-{
+Camera::Camera(glm::vec3 position, glm::vec3 target, glm::vec3 worldup) {
 	Position = position;
 	WorldUp = worldup;
 	Forward = glm::normalize(target - position);
 	Right = glm::normalize(glm::cross(Forward, WorldUp));
 	Up = glm::normalize(glm::cross(Forward, Right));
-
+	Pitch = glm::radians(15.0f);
+	Yaw = glm::radians(180.0f);
 }
 
 Camera::Camera(glm::vec3 position, float pitch, float yaw, glm::vec3 worldup) {
@@ -24,9 +23,7 @@ Camera::Camera(glm::vec3 position, float pitch, float yaw, glm::vec3 worldup) {
 	Up = glm::normalize(glm::cross(Right, Forward));
 }
 
-
-Camera::~Camera()
-{
+Camera::~Camera() {
 }
 
 glm::mat4 Camera::GetViewMatrix() {
@@ -43,11 +40,17 @@ void Camera::UpdateCameraVectors() {
 
 void Camera::ProcessMouseMovement(float deltaX, float deltaY) {
 	Pitch -= deltaY * SenseX;
+	if (Pitch > glm::radians(89.0f)) {
+		Pitch = glm::radians(89.0f);
+	}
+	if (Pitch < glm::radians(- 89.0f)) {
+		Pitch = glm::radians(- 89.0f);
+	}
+	std::cout << "Pitch: " << Pitch << std::endl;
 	Yaw -= deltaX * SenseY;
 	UpdateCameraVectors();
 }
 
 void Camera::UpdateCameraPos() {
 	Position += Forward * speedZ * 0.1f + Right * speedX * 0.1f + Up * speedY * 0.1f;
-	//Position -= Right * speedX * 0.1f;
 }
